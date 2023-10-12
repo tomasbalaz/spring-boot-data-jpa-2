5abff14ed61456f6a062779a1f75d47b8f3e2a35
Tomas Balaz <balaztomas0@gmail.com>
2023-10-12 14:34:27 +0200

    Created application spring-boot-data-jpa-2

---
+++ src/test/java/com/example/springbootdatajpa/SpringBootDataJpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
