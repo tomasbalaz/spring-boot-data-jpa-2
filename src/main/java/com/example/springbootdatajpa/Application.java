312f66351089aae3ac8e6eecffeccd8e5c7f39aa
Tomas Balaz <balaztomas0@gmail.com>
2023-10-13 12:54:14 +0200

    Created application spring-boot-data-jpa-2

--- src/main/java/com/example/springbootdatajpa/Application.java
+++ src/main/java/com/example/springbootdatajpa/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDataJpa2Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDataJpa2Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
