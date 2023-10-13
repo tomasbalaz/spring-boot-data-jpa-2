c4aec83f4d27c2893fbcac16c6378cfaa0a01b6c
Tomas Balaz <balaztomas0@gmail.com>
2023-10-13 14:28:11 +0200

    Annotated Student class with @UniqueConstraint and add constraint to column email

--- src/main/java/com/example/springbootdatajpa/Student.java
+++ src/main/java/com/example/springbootdatajpa/Student.java
@@ -3,6 +3,12 @@ package com.example.springbootdatajpa;
 import jakarta.persistence.*;
 
 @Entity(name = "Student")
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        }
+)
 public class Student {
     @Id
     @SequenceGenerator(
@@ -34,8 +40,7 @@ public class Student {
     @Column(
             name = "email",
             nullable = false,
-            columnDefinition = "TEXT",
-            unique = true
+            columnDefinition = "TEXT"
     )
     private String email;
     @Column(
@@ -54,6 +59,10 @@ public class Student {
         this.lastName = lastName;
         this.email = email;
         this.age = age;
+    }
+
+    public Student() {
+
     }
 
     public Long getId() {
