c57a779afa47d6140b222edbb9cd91909f9947d1
Tomas Balaz <balaztomas0@gmail.com>
2023-10-17 14:50:01 +0200

    Add @Query Annotation and examples for it

--- src/main/java/com/example/springbootdatajpa/StudentRepository.java
+++ src/main/java/com/example/springbootdatajpa/StudentRepository.java
@@ -1,16 +1,19 @@
 package com.example.springbootdatajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age = ?2")
     List<Student> findStudentsByFirstNameEqualsAndAgeEquals(
             String firstName, Integer age);
-
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
     List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThan(
             String firstName, Integer age);
 }
