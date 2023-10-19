75ec79bcb40e1c2f904c22ce78503c7441a46b94
Tomas Balaz <balaztomas0@gmail.com>
2023-10-19 13:53:49 +0200

    Example of Sort in JPA

--- src/main/java/com/example/springbootdatajpa/Application.java
+++ src/main/java/com/example/springbootdatajpa/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 import java.util.stream.IntStream;
 
@@ -17,7 +18,20 @@ public class Application {
 
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
-		return args -> IntStream.rangeClosed(1 ,20)
+		return args -> {
+			generateRandomStudents(studentRepository);
+
+			//Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
+			Sort sort = Sort.by("firstName").ascending()
+					.and(Sort.by("age")).descending();
+
+			studentRepository.findAll(sort)
+					.forEach(student -> System.out.println(student.getFirstName() + " " + student.getLastName()));
+		};
+	}
+
+	private void generateRandomStudents(StudentRepository studentRepository) {
+		IntStream.rangeClosed(1 ,20)
 				.forEach(value -> {
 					Faker faker = new Faker();
 					String firstName = faker.name().firstName();
