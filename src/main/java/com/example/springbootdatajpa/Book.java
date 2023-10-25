9e5f69aa04d65aa32ed28bcca171fa7dd4ed7d5e
Tomas Balaz <balaztomas0@gmail.com>
2023-10-25 13:54:25 +0200

    Created Book class and add @ManyToOne Annotation

---
+++ src/main/java/com/example/springbootdatajpa/Book.java
@@ -0,0 +1,103 @@
+package com.example.springbootdatajpa;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "Book")
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @SequenceGenerator(
+            name = "book_sequence",
+            sequenceName = "book_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "book_id_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "book_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+
+    )
+    private String bookName;
+
+    @Column(
+            name = "created_at",
+            nullable = false,
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
+    )
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "student_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "student_book_fk"
+            )
+    )
+    private Student student;
+
+    public Book() {
+    }
+
+    public Book(String bookName, LocalDateTime createdAt, Student student) {
+        this.bookName = bookName;
+        this.createdAt = createdAt;
+        this.student = student;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookName='" + bookName + '\'' +
+                ", createdAt=" + createdAt +
+                ", student=" + student +
+                '}';
+    }
+}
