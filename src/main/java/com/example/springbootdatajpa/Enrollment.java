0946563eab07addbe866277efaa2a391e89ca30b
Tomas Balaz <balaztomas0@gmail.com>
2023-10-30 14:16:04 +0100

    Created Enrolment class

---
+++ src/main/java/com/example/springbootdatajpa/Enrollment.java
@@ -0,0 +1,52 @@
+package com.example.springbootdatajpa;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Enrollment")
+@Table(name = "enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentId id;
+
+    @ManyToOne
+    @MapsId("studentId")
+    @JoinColumn(name = "student_id")
+    private Student student;
+    @ManyToOne
+    @MapsId("courseId")
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public EnrollmentId getId() {
+        return id;
+    }
+
+    public void setId(EnrollmentId id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
